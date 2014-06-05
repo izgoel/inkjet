520b1f8c5d99a7d5d04be6befc328b3e295d0b75
Administrator <Administrator@SKY-20140306JLM>
2014-06-05 18:52:27 +0700

    First changes

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -20,4 +20,13 @@ class Controller extends CController
 	 * for more details on how to specify this property.
 	 */
 	public $breadcrumbs=array();
+        
+        public function init(){
+            //Yii::import('ext.LangPick.ELangPick'); 
+            //ELangPick::setLanguage();
+            //Yii::app()->setLanguage('th');
+            parent::init();
+            
+        }
+        
 }
