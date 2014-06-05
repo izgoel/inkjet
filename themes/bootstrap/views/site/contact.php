5d27022e4783ecd97b6f3e743ae07d30b23d6b77
Administrator <Administrator@SKY-20140306JLM>
2014-06-05 16:21:11 +0700

    First changes

--- themes/bootstrap/views/site/contact.php
+++ themes/bootstrap/views/site/contact.php
@@ -9,7 +9,7 @@ $this->breadcrumbs=array(
 );
 ?>
 <div class="row-fluid">
-    <div class="span8">
+    <div class="span6">
 <h2><?php echo Yii::t('messages','Contact Us')?></h2>
 
 <?php if(Yii::app()->user->hasFlash('contact')): ?>
@@ -64,13 +64,16 @@ $this->breadcrumbs=array(
 </div><!-- form -->
 
 <?php endif; ?>
-</div><!--span8-->
-<div class="span4">
+</div><!--span6-->
+<div class="span6">
     <h2><?php echo Yii::t('messages','Our location')?></h2>
-    <img src="images/location.jpg" style="border: 1px solid" />
-    <p><?php echo Yii::t('messages','You can also send us an email')?>
-        <a href="mailto:office@print-design.tk?subject=Inquiry">office@print-design.tk</a></p>
+   <!-- <img src="images/location.jpg" style="border: 1px solid" />
+    <p><?php //echo Yii::t('messages','You can also send us an email')?>
+        <a href="mailto:office@print-design.tk?subject=Inquiry">office@print-design.tk</a></p>-->
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3876.0053038042915!2d100.79975583467102!3d13.718128437119827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311d67d5ba67f0df%3A0x2ef286db22cf421b!2s625+Thanon+Luang+Phaeng!5e0!3m2!1sen!2s!4v1401959153581" 
+        width="425" height="350" frameborder="0" style="border:0"></iframe>    
 </div>
 
 
 </div><!--row fluid-->
+
