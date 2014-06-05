ca241aff0e740d5d9a641377028819b3ddbbd3d8
Administrator <Administrator@SKY-20140306JLM>
2014-06-05 14:50:07 +0700

    First commit 5.6.2014, from shop

---
+++ themes/bootstrap/views/layouts/footer.php
@@ -0,0 +1,21 @@
+	<div id="footer">
+            <div class="row-fluid">
+                <div class="span6">
+                    <p><b>INKJET LADKRABANG</b></p>
+                    <p> 621 หลวงแพ่ง  แขวงทับยาว </br> เขตลาดกระบัง  กทม  10510 </p>
+                    <!--<p><b>PRINT DESIGN</b> </br>300/30 On Nut Road </br>10520 Lat Krabang </br>
+                    Mobile: 0847 084288</p>-->
+                    
+                </div>
+                <div class="span3">
+                    
+                </div>
+                <div class="span3">
+                    Copyright &copy; <?php echo date('Y'); ?> <a href="http://misoweb.tk/index.php?id='NN01'" target="_blank">
+                                     <b>MISO Web Design</b></a><br/>
+		All Rights Reserved.<br/>
+                <p>Powered by Yii Framework,</br> hosted by <b>MISO hosting</b></p>
+                </div>
+
+            </div>
+	</div><!-- footer -->
