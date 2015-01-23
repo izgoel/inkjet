343caca66409743909066244c6f9547254e5d3af
Administrator <Administrator@SKY-20140306JLM>
2015-01-23 14:14:27 +0700

    First changes

--- themes/bootstrap/views/layouts/slider.php
+++ themes/bootstrap/views/layouts/slider.php
@@ -3,17 +3,17 @@
     <div class="row-fluid">
         <div class="slider" style="margin-left: 10px;">
         <div class="span4" id="s1" style="height:220px;">
-            <img src="images/graphic-design-1.png" width="300" height="210"/>
-            <img src="images/stamps-3.png" width="300" height="210"/>
+            <img src="images/slider-01_01.png" width="300" height="210"/>
+            <img src="images/slider-02_01.png" width="300" height="210"/>
         </div>
         <div class="span4" id="s2" style="height:220px;">
-            <img src="images/graphic-design-2.png" />
-            <img src="images/graphic-design-3.png" />
+            <img src="images/slider-01_02.png" />
+            <img src="images/slider-02_02.png" />
 
         </div>
         <div class="span4" id="s3" style="height:220px;">
-            <img src="images/graphic-design-3.png" />
-            <img src="images/stamp-offset-1.png" />
+            <img src="images/slider-01_03.png" />
+            <img src="images/slider-02_03.png" />
         </div>
     </div>
     <script type="text/javascript">
